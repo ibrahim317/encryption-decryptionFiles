9391b6d8edf26656ea861e7cb0b1e14963a99974
ibrahim317 <74654255+ibrahim317@users.noreply.github.com>
2021-05-23 14:59:28 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include "header.h"
 using namespace std;
 
 int main()
@@ -9,89 +10,26 @@ int main()
     ofstream outfile;
     string spath;
     string tpath;
-    int x;
-    cout << "\n\nType 1 to encrypt your file \nType 2 to decrypt your file \nType 3 to exit\n";
-    for (int i = 0; i < 80; i++)
-    {
-        cout << "-";
-    }
-    cout << "\n Enter your choose :  ";
-    cin >> x;
+    list();
     while (x != 3)
     {
         if (x == 1)
         {
-            for (int i = 0; i < 80; i++)
-            {
-                cout << "-";
-            }
-            cout << "\n inter the path of the file that you want to encrypt : ";
-            cin >> spath;
-            infile.open(spath);
-            while (infile.fail())
-            {
-                cout << "can't find the file in the path that you intered please cheak the name";
-                cout << "\n inter the path of the file that you want to encrypt : ";
-                cin >> spath;
-                infile.open(spath);
-            }
-            cout << "\n inter the path of the target file : ";
-            cin >> tpath;
-            outfile.open(tpath);
-            string contant;
-            for (int i = 0; infile.eof() != true; i++)
-            {
-                contant += infile.get() + '*';
-            }
-            outfile << contant;
-            infile.close();
-            outfile.close();
+            line();
+            openFiles();
+            encrypt();
             cout << "\n\n\t\t\t#Encryption is Done Succefully#";
-            cout << "\n\nType 1 to encrypt your file \nType 2 to decrypt your file \nType 3 to exit\n";
-            for (int i = 0; i < 80; i++)
-            {
-                cout << "-";
-            }
-            cout << "\n Enter your choose :  ";
-            cin >> x;
+            list();
         }
         if (x == 2)
         {
-            for (int i = 0; i < 80; i++)
-            {
-                cout << "-";
-            }
-            cout << "\n inter the path of the file that you want to decrypt : ";
-            cin >> spath;
-            infile.open(spath);
-            while (infile.fail())
-            {
-                cout << "can't find the file in the path that you intered please cheak the name";
-                cout << "\n inter the path of the file that you want to decrypt : ";
-                cin >> spath;
-                infile.open(spath);
-            }
-            cout << "\n inter the path of the target file : ";
-            cin >> tpath;
-            outfile.open(tpath);
-            string contant;
-            for (int i = 0; infile.eof() != true; i++)
-            {
-                contant += infile.get() - '*';
-            }
-            outfile << contant;
-            infile.close();
-            outfile.close();
+            line();
+            openFiles();
+            decrypt();
             cout << "\n\n\t\t\t#Decryption is Done Succefully#";
-            cout << "\n\nType 1 to encrypt your file \nType 2 to decrypt your file \nType 3 to exit\n";
-            for (int i = 0; i < 80; i++)
-            {
-                cout << "-";
-            }
-            cout << "\n Enter your choose :  ";
-            cin >> x;
+            list();
         }
-        if (x != 1 && (x != 2) || (x != 3))
+        if (x != 3 && (x != 2 && x != 1))
         {
             cout << "You have entered an invalid value,Enter your choose :  ";
             cin >> x;
