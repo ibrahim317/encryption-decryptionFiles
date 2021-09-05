1b789824590ebb84abb3144f0c0455b198da3c85
mrzero <mrzero@localhost.localdomain>
2021-09-05 13:33:41 +0200

    the bugs has been removed

--- home.h
+++ home.h
@@ -18,8 +18,6 @@ public:
 private slots:
     void on_pushButton_clicked();
     void on_pushButton_2_clicked();
-    void on_pushButton_3_clicked();
-    void on_pushButton_5_clicked();
 private:
     Ui::home *ui;
 };
